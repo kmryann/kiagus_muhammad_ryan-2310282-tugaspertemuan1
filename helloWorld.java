0b11a4fceb083f64085bd63413603a86bc90251c
Ryan <kmryan1345@gmail.com>
2025-02-11 21:30:06 +0700

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,6 @@
+public class helloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World, Kiagus Muhammad Ryan 2310282, Saya Senang Belajar Java");
+    }
+}
